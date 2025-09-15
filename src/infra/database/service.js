0b4b9f5016cf4f9bf4dfbeaa8e6be5882ceb7097
koakovski <hkoakovskip@gmail.com>
2025-09-15 20:08:59 -0300

    refactor: simplify database connection setup by using centralized configuration

--- src/infra/database/service.js
+++ src/infra/database/service.js
@@ -1,21 +1,9 @@
 import { Sequelize } from "sequelize";
-import { config } from "dotenv";
+import sequelizeConfig from "./config.cjs";
 
-config();
-
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASS;
-const dbHost = process.env.DB_HOST || "localhost";
-const dbPort = process.env.DB_PORT || 3306;
-const dbDialect = "mysql";
-const dbLogging = process.env.NODE_ENV === "development" ? console.log : false;
-
-const sequelize = new Sequelize(dbName, dbUser, dbPass, {
-  host: dbHost,
-  port: dbPort,
-  dialect: dbDialect,
-  logging: dbLogging,
+const sequelize = new Sequelize(sequelizeConfig.url, {
+  dialect: sequelizeConfig.dialect,
+  logging: sequelizeConfig.logging,
   dialectOptions: {
     connectTimeout: 60000,
   },
