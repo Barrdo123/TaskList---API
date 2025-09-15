2dadfb6a361d443e311948a34118af1eba81bc2a
koakovski <hkoakovskip@gmail.com>
2025-09-15 20:06:49 -0300

    refactor: restructure database configuration and update migration file to ES module syntax

---
+++ src/infra/database/config.cjs
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+function getDatabaseUrl() {
+  if (process.env.DATABASE_URL) return process.env.DATABASE_URL;
+
+  const dbUser = process.env.DB_USER;
+  const dbPass = process.env.DB_PASS;
+  const dbHost = process.env.DB_HOST || "localhost";
+  const dbPort = process.env.DB_PORT || 3306;
+  const dbName = process.env.DB_NAME;
+
+  return `mysql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
+}
+
+module.exports = {
+  url: getDatabaseUrl(),
+  dialect: "mysql",
+  logging: console.log,
+};
